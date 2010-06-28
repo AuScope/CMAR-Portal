18b326af774b43b7c84eb7691e4a9e085d87a325
Josh Vote <Josh.Vote@csiro.au>
2010-06-28 03:26:55 +0000

    Added JFreeChart dependency and CTD Plot skeleton code

---
+++ src/main/java/org/auscope/portal/server/web/controllers/CTDPlotController.java
@@ -0,0 +1,17 @@
+package org.auscope.portal.server.web.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CTDPlotController {
+	
+	
+	    @RequestMapping("/getCTDPlot.do")
+	    public void gridLogin(HttpServletRequest request) {
+	    	
+	    }
+
+}
